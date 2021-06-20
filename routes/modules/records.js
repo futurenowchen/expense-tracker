0d7f52f9687250bc5944a640aed3770ce3c76753
futurenowchen <futurenowchen@gmail.com>
2021-06-20 16:11:21 +0800

    feat: add edit function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/Record')
 
-//渲染新增支出記錄頁面路由
+//新增支出記錄頁面路由
 router.get('/new', (req, res) => {
   return res.render('new')
 })
 
+//新增支出記錄功能路由
 router.post('/', (req, res) => {
   const newRecord = req.body
   return Record.create({
@@ -17,6 +18,14 @@ router.post('/', (req, res) => {
   })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+
+//修改支出記錄頁面路由
+router.get('/:record_id/edit', (req, res) => {
+  const id = req.params.record_id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
 })
 
 module.exports = router
