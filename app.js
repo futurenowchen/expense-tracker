40e7085fda31db1a0a9c674182a650882c944893
futurenowchen <futurenowchen@gmail.com>
2021-08-15 00:33:54 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const userPassport = require('./config/passport')
 const routes = require('./routes')
 require('./config/mongoose')
@@ -15,7 +18,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
