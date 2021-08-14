3d4933fefbd78ca41018bd39d15d139d08f18976
futurenowchen <futurenowchen@gmail.com>
2021-08-15 00:22:55 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
 
